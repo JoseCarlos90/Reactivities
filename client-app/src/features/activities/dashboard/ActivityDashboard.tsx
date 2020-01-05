aad00dd910dd1bf03a40512b8f0cdd94b89a3761
Jose Carlos Gonzalez Horcajo <jossu_sw000@hotmail.com>
2020-01-05 11:12:50 +0100

    End of section 8

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,31 +1,31 @@
-import React, {  useContext } from 'react'
-import { Grid, GridColumn } from 'semantic-ui-react'
-import { observer } from 'mobx-react-lite';
-import ActivityList from './ActivityList'
-import ActivityStore from '../../../app/stores/activityStore'
-import ActivityDetails from '../details/ActivityDetails';
-import ActivityForm from '../form/ActivityForm';
+import React, { useContext, useEffect } from "react";
+import { Grid, GridColumn } from "semantic-ui-react";
+import { observer } from "mobx-react-lite";
+import ActivityList from "./ActivityList";
+import ActivityStore from '../../../app/stores/activityStore';
+import LoadingComponent from "../../../app/layout/LoadingComponent";
 
 const ActivityDashboard: React.FC = () => {
-     const activityStore = useContext(ActivityStore);
-     const {editMode, selectedActivity} = activityStore;
-    return (
-        <Grid>
-            <GridColumn width={10}>
-                <ActivityList/>
-            </GridColumn>
-            <GridColumn width={6}>
-                {selectedActivity && !editMode &&
-                    <ActivityDetails/>
-                }
 
-                {editMode && <ActivityForm
-                    key={(selectedActivity && selectedActivity.id) || 0}
-                    activity={selectedActivity!}
-                    />}
-            </GridColumn>
-        </Grid>
-    )
-}
+  const activityStore = useContext(ActivityStore);
+
+  useEffect(() => {
+    activityStore.loadActivities();
+  }, [activityStore]);
+
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content="Loading activities..." />;
+
+  return (
+    <Grid>
+      <GridColumn width={10}>
+        <ActivityList />
+      </GridColumn>
+      <GridColumn width={6}>
+        <h2>Activity Filters</h2>
+      </GridColumn>
+    </Grid>
+  );
+};
 
 export default observer(ActivityDashboard);
